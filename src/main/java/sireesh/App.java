b07731682c1d6dc9da080241c2942840d9222d82
root <root@ip-172-31-1-205.ap-south-1.compute.internal>
2019-11-10 14:25:32 +0000

    sample project

---
+++ src/main/java/sireesh/App.java
@@ -0,0 +1,13 @@
+package sireesh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! and how are you");
+    }
+}
